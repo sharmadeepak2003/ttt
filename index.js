cc83fb5587cad268513f1d7b5a61c3f0d1283cdc
sharmadeepak2003 <sharmadeepak13872@gmail.com>
2026-04-18 14:35:16 +0530

    Add DNS server configuration and update CORS origins

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.js";
 import noteRoutes from "./routes/notes.js";
+import dns from "dns";
 
 dotenv.config();
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      process.env.FRONTEND_URL,
+      process.env.FRONTEND_URL_NETLIFY,
+      process.env.FRONTEND_URL_VERCEL,
+    ],
     credentials: true,
   }),
 );
@@ -25,6 +32,9 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
+app.use("/", (req, res) => {
+  res.send("Hello World welcome to my The True Topper API");
+});
 
 const PORT = process.env.PORT || 8000;
 
